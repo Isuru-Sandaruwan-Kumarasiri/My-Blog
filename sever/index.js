9951971359fdb2468126f002226035509d73bad5
Isuru-Sandaruwan-Kumarasiri <s20426@sci.pdn.ac.lk>
2024-02-23 09:14:24 +0530

    created routes  sever file and connect postman

--- sever/index.js
+++ sever/index.js
@@ -3,13 +3,17 @@ const cors = require('cors');
 const { connect } = require('mongoose');
 require('dotenv').config();
 
+
+const userRoutes=require('./routes/postRoutes')
+const postRoutes=require('./routes/userRoutes')
+
 const app = express();
 app.use(express.json({extended:true}));
 app.use(express.urlencoded({extended:true}))
 app.use(cors({credentials:true,origin:"http://localhost:3000"}))
 
-// app.use("/api/users",userRoutes)
-// app.use("/api/posts",postRoutes)
+app.use("/api/users",userRoutes)
+app.use("/api/posts",postRoutes)
 
 connect(process.env.MONGO_URL)
   .then(() => app.listen(process.env.PORT || 5000, () => console.log(`Server running on port ${process.env.PORT || 5000}`)))
