19dc50553726ceb6436b4546985ff75031fdf506
Isuru-Sandaruwan-Kumarasiri <s20426@sci.pdn.ac.lk>
2024-02-23 08:01:53 +0530

    cearting sever routes

--- sever/index.js
+++ sever/index.js
@@ -4,7 +4,13 @@ const { connect } = require('mongoose');
 require('dotenv').config();
 
 const app = express();
+app.use(express.json({extended:true}));
+app.use(express.urlencoded({extended:true}))
+app.use(cors({credentials:true,origin:"http://localhost:3000"}))
+
+// app.use("/api/users",userRoutes)
+// app.use("/api/posts",postRoutes)
 
 connect(process.env.MONGO_URL)
   .then(() => app.listen(process.env.PORT || 5000, () => console.log(`Server running on port ${process.env.PORT || 5000}`)))
-  .catch(error => console.log(error))
+  .catch(error => console.log(error));
