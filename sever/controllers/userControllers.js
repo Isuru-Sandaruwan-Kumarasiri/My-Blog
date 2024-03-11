ed0b4d9ee8856607d6d0d5b950e59327564ea185
Isuru-Sandaruwan-Kumarasiri <s20426@sci.pdn.ac.lk>
2024-03-11 23:33:39 +0530

    completed backend part of registerUser

--- sever/controllers/userControllers.js
+++ sever/controllers/userControllers.js
@@ -1,10 +1,57 @@
+const bcrypt=require('bcryptjs');
+
+
+const User =require ("../models/userModel");
+const HttpError=require("../models/errorModel");
+
+
+
+
 //***********************REGISTER A NEW USER
 //POST:api/users/register
 //UNPROTECTED
 
 const registerUser=async(req,res,next)=>{
-    res.json("Register user")
+    try {
+        const {name,email,password,password2}=req.body;
+        
+        if(!name || !email || !password){
+            return next (new HttpError ("Fill in all fields ",422));
+        }
+
+        const newEmail=email.toLowerCase();
+        const emailExists =await User.findOne({email:newEmail});
+        if(emailExists){
+            return next(new HttpError ("Email already exists ",422))
+        }
+        if((password.trim()).lenght<6){
+            return next(new HttpError ("Password should be at least 6 charactoers",422)) 
+        }
+        if(password!=password2){
+            return next(new HttpError ("Password do not match ",422))
+        }
+
+        const salt=await bcrypt.genSalt(10);
+        const hashePass=await bcrypt.hash(password,salt);
+        const newUser=await User.create({name,eamil:newEmail,password:hashePass})
+        res.status(201).json(newUser)
+
+    } catch (error) {
+       return next (new HttpError("User registration failed",422)) 
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
