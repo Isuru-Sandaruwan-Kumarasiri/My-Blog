d49ff19ecc5421cb8f91d11a1186bd19b95cc180
Isuru-Sandaruwan-Kumarasiri <s20426@sci.pdn.ac.lk>
2024-03-14 23:48:20 +0530

    changed router

--- sever/routes/userRoutes.js
+++ sever/routes/userRoutes.js
@@ -1,5 +1,6 @@
 const {Router}=require('express');
 const {registerUser,loginUser,getUser,changeAvatar,editUser,getAuthor}=require("../controllers/userControllers")
+const authMiddleware=require("../middleware/authMiddleware")
 
 const router=Router()
 
@@ -11,7 +12,7 @@ router.post("/register",registerUser)
 router.post("/login",loginUser)
 router.get("/:id",getUser)
 router.get("/",getAuthor)
-router.post("/change-avatar",changeAvatar)
+router.post("/change-avatar",authMiddleware,changeAvatar)
 router.patch("/edit-user",editUser)
 
 
